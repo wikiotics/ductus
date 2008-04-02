2392c8a0543d256373932c4b81dbf6a8cf6820eb
Jim Garrison <jim@garrison.cc>
2008-04-02 01:11:37 -0400

    Files will now be opened in binary mode ("rb") in non-Unix environments

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 
 def iterate_file(filename):
-    with file(filename) as f:
+    with file(filename, 'rb') as f:
         while True:
             x = f.read(4096)
             if x == '':
