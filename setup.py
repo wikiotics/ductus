4c9bc0125d554e3d994129f378c84e852db07afa
Jim Garrison <jim@garrison.cc>
2008-04-02 23:13:35 -0400

    added distutils build system

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ductus',
+      version='0.1pre',
+      description='Wikiotics wiki system',
+      license='GNU GPLv3 or later',
+      author='Jim Garrison',
+      author_email='jim@garrison.cc',
+      url='http://wikiotics.org/',
+      packages=['ductus', 'ductus.storage'],
+     )
