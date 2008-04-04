052a468e6b2aeafcf64b471c30de40602247c3a0
Jim Garrison <jim@garrison.cc>
2008-04-04 00:43:47 -0400

    Added null storage backend for testing purposes

---
+++ ductus/storage/null.py
@@ -0,0 +1,22 @@
+class NullStorageBackend(object):
+    """Pointless storage backend.  Always contains nothing.
+
+    May actually be useful for testing other backends, such as
+    UnionStorageBackend.
+    """
+
+    def __contains__(self, key):
+        return False
+    def __setitem__(self, key, value):
+        raise Exception("Can't save anything to the null storage backend")
+    def __getitem__(self, key):
+        raise KeyError(key)
+    def __delitem__(self, key):
+        raise KeyError(key)
+    def keys(self):
+        return []
+    def iterkeys(self):
+        return iter([])
+    __iter__ = iterkeys
+    def __len__(self):
+        return 0
