74bfee486fa7e22aff13003e4d1b83f7c278543b
Jim Garrison <jim@garrison.cc>
2008-04-12 03:29:20 -0400

    ductus.apps.urn: fixed import of DUCTUS_STORAGE_BACKEND

--- ductus/apps/urn/__init__.py
+++ ductus/apps/urn/__init__.py
@@ -4,7 +4,8 @@ def get_resource_database():
         from django.conf import settings
         backend = settings.DUCTUS_STORAGE_BACKEND
         mod_name, junk, var_name = backend.rpartition('.')
-        __resource_database = getattr(__import__(mod_name), var_name)
+        __resource_database = getattr(__import__(mod_name, {}, {}, ['']),
+                                      var_name)
     return __resource_database
 
 __resource_database = None
