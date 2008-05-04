1a9fab855ca45dfac8c08aa31bda64072fc3fa09
Jim Garrison <jim@garrison.cc>
2008-05-03 20:44:09 -0400

    preliminary image resizing support

--- ductus/applets/picture/views.py
+++ ductus/applets/picture/views.py
@@ -2,6 +2,11 @@ from django.http import HttpResponse
 from ductus.apps.urn.views import register_view
 from ductus.apps.urn import get_resource_database
 
+from PIL import Image, ImageFile
+from cStringIO import StringIO
+
+__allowed_thumbnail_sizes = set([(250, 250)])
+
 @register_view('{http://wikiotics.org/ns/2008/picture}picture', None)
 def view_picture(request, requested_view, urn, tree):
     blob = tree.getroot().find('{http://wikiotics.org/ns/2008/picture}blob')
@@ -13,7 +18,28 @@ def view_picture(request, requested_view, urn, tree):
 
     # fixme: set X-License header
 
-    # send blob as image
+    # prepare original image
     data_iterator = get_resource_database().get_blob(blob_urn)
+
+    # resize if requested
+    if 'max_size' in request.GET:
+        try:
+            max_width, max_height = [int(n) for n in
+                                     request.GET['max_size'].split(',')]
+        except Exception:
+            raise # call this a formatting error or something
+        if (max_width, max_height) not in __allowed_thumbnail_sizes:
+            raise Exception("Requested size not available")
+
+        p = ImageFile.Parser()
+        for data in data_iterator:
+            p.feed(data)
+        im = p.close()
+        im.thumbnail((max_width, max_height), Image.ANTIALIAS)
+        output = StringIO()
+        im.save(output, 'JPEG')
+        data_iterator = iter([output.getvalue()])
+        mime_type = 'image/jpeg'
+
     return HttpResponse(data_iterator,
                         content_type=mime_type)
