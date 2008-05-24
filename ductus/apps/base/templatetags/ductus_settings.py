8ee6b8072bcd9a1a376b74abe0370c342c4fbe34
Jim Garrison <jim@garrison.cc>
2008-05-23 21:50:41 -0400

    ductus_settings template tags

---
+++ ductus/apps/base/templatetags/ductus_settings.py
@@ -0,0 +1,28 @@
+# Ductus
+# Copyright (C) 2008  Jim Garrison <jim@garrison.cc>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.template import Library
+from django.conf import settings
+
+register = Library()
+
+@register.simple_tag
+def ductus_media_prefix():
+    return settings.DUCTUS_MEDIA_PREFIX
+
+@register.simple_tag
+def ductus_site_name():
+    return settings.DUCTUS_SITE_NAME
