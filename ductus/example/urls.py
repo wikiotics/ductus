861aa070bfa6e2b807ffff8c2f479c2c14045993
Jim Garrison <jim@garrison.cc>
2008-07-13 23:34:44 -0400

    Work on login/logout mechanism

--- ductus/example/urls.py
+++ ductus/example/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     (r'^urn/', include('ductus.apps.urn.urls')),
 #    (r'^user/', include('ductus.apps.user.urls')),
 #    (r'^wiki/', include('ductus.apps.wiki.urls')),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
     (r'^new/picture/', 'ductus.applets.picture.edit_views.new_picture'),
     (r'^new/picture_choice/', 'ductus.applets.picture_choice.edit_views.new_picture_choice'),
 )
