96313bb4a20c37f018355cb05f059f491c22058a
Jim Garrison <jim@garrison.cc>
2008-10-06 16:58:46 -0400

    significant improvements to login mechanism

--- ductus/example/settings.py
+++ ductus/example/settings.py
@@ -74,6 +74,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.request',
     'ductus.context_processors.site_settings',
 )
 
@@ -107,6 +108,7 @@ CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
 
 LOGIN_URL = '/login/'
 LOGOUT_URL= '/logout/'
+LOGIN_REDIRECT_URL = '/'
 
 try:
     from ductus_local_settings import *
