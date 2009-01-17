d6aea4bb5d840bd439a3844917c8615e0833dd99
Jim Garrison <jim@garrison.cc>
2009-01-17 08:12:33 -0500

    ability for users to change passwords

--- ductus/user/views.py
+++ ductus/user/views.py
@@ -1,15 +1,18 @@
 from django.views.decorators.vary import vary_on_headers
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.contrib.auth.views import login as _login, logout as _logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 from django.contrib.auth.forms import UserCreationForm
 
 def wrap(view):
     return vary_on_headers('Cookie', 'Accept-language')(view)
 
-login = wrap(_login)
-logout = wrap(_logout)
+login = wrap(login)
+logout = wrap(logout)
+password_change = wrap(password_change)
+password_change_done = wrap(password_change_done)
 
+@wrap
 def user_creation(request, template_name='registration/create_user.html',
                   success_template_name='registration/user_created.html'):
     "Displays user creation form and handles its action"
