08239eadf009420ef636a638559ffde26de132c4
Jim Garrison <jim@garrison.cc>
2009-01-17 05:37:29 -0500

    user creation

--- ductus/user/views.py
+++ ductus/user/views.py
@@ -1,8 +1,28 @@
 from django.views.decorators.vary import vary_on_headers
-import django.contrib.auth.views
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth.views import login as _login, logout as _logout
+from django.contrib.auth.forms import UserCreationForm
 
 def wrap(view):
     return vary_on_headers('Cookie', 'Accept-language')(view)
 
-login = wrap(django.contrib.auth.views.login)
-logout = wrap(django.contrib.auth.views.logout)
+login = wrap(_login)
+logout = wrap(_logout)
+
+def user_creation(request, template_name='registration/create_user.html',
+                  success_template_name='registration/user_created.html'):
+    "Displays user creation form and handles its action"
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render_to_response(success_template_name, {
+                'username': form.cleaned_data["username"],
+            }, context_instance=RequestContext(request))
+    else:
+        form = UserCreationForm()
+
+    return render_to_response(template_name, {
+        'form': form,
+    }, context_instance=RequestContext(request))
