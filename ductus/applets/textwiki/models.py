699148da187185808a35a8d97cfd8762f9b5bbd9
Jim Garrison <jim@garrison.cc>
2009-02-09 07:41:44 -0500

    Wikitext model now saves which markup language is used in a resource (currently always set to "creole-1.0")

--- ductus/applets/textwiki/models.py
+++ ductus/applets/textwiki/models.py
@@ -1,9 +1,19 @@
 from ductus.resource import models, register_model
 from ductus.util import create_property
 
+class WikiBlobElement(models.BlobElement):
+    allowed_languages = ('creole-1.0',)
+    markup_language = models.Attribute(validator=models.allowed_values_attribute_validator(allowed_languages))
+
+    def __init__(self):
+        # fixme: if Attribute had a "default" argument, we wouldn't need to
+        # override this constructor to set such a default
+        super(WikiBlobElement, self).__init__()
+        self.markup_language = "creole-1.0"
+
 class Wikitext(models.Model):
     ns = 'http://wikiotics.org/ns/2009/wikitext'
-    blob = models.BlobElement()
+    blob = WikiBlobElement()
 
     @create_property
     def text():
