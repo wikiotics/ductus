8d51c0d63f24464350b4d99e156b95edaa0038a3
Jim Garrison <jim@garrison.cc>
2009-02-12 06:29:57 -0500

    updated setup.py, but the distribution still does not include data files

--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
-# this file is much out of date
+# this file is likely to be out of date at any given time
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-setup(name='Ductus',
-      version='0.1pre',
-      description='Wikiotics wiki system',
-      license='GNU GPLv3 or later',
-      author='Jim Garrison',
-      author_email='jim@garrison.cc',
-      url='http://wikiotics.org/',
-      packages=['ductus',
-                'ductus.resource',
-                'ductus.resource.storage',
-                ],
-     )
+setup(
+    name='Ductus',
+    version='0.1pre',
+    description='Wikiotics wiki system',
+    license='GNU GPLv3 or later',
+    author='Jim Garrison',
+    author_email='jim@garrison.cc',
+    url='http://wikiotics.org/',
+    install_requires=(
+        'Django >= 1.0',
+        'Creoleparser',
+    ),
+    extras_require={
+        'flickr': ['flickrapi >= 1.2'],
+        'highlighting': ['pygments'],
+    },
+    packages=(
+        'ductus',
+        'ductus.util',
+        'ductus.resource',
+        'ductus.resource.storage',
+        'ductus.wiki',
+        'ductus.user',
+        'ductus.template_loaders',
+        'ductus.middleware',
+        'ductus.applets',
+        'ductus.applets.picture',
+        'ductus.applets.picture_choice',
+        'ductus.applets.picture_choice_lesson',
+        'ductus.applets.textwiki',
+        'ductus.applets.textwiki.templatetags',
+    ),
+)
