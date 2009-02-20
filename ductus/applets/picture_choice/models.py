11761e7f6fe4043efa434a37b0c62f18f847a98c
Jim Garrison <jim@garrison.cc>
2009-02-20 02:21:14 -0500

    now registers PictureChoice model correctly

--- ductus/applets/picture_choice/models.py
+++ ductus/applets/picture_choice/models.py
@@ -1,7 +1,8 @@
-from ductus.resource import models
+from ductus.resource import models, register_model
 
 class PictureChoice(models.Model):
     ns = 'http://wikiotics.org/ns/2009/picture_choice'
     phrase = models.TextElement()
     correct_picture = models.ResourceElement()
     incorrect_pictures = models.ArrayElement(models.ResourceElement())
+register_model(PictureChoice)
