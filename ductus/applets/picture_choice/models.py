6c1e8f974afef5d8527f098386be9a6009caeb2f
Jim Garrison <jim@garrison.cc>
2009-02-20 03:55:38 -0500

    force each PictureChoice to have precisely 3 incorrect pictures

--- ductus/applets/picture_choice/models.py
+++ ductus/applets/picture_choice/models.py
@@ -4,5 +4,6 @@ class PictureChoice(models.Model):
     ns = 'http://wikiotics.org/ns/2009/picture_choice'
     phrase = models.TextElement()
     correct_picture = models.ResourceElement()
-    incorrect_pictures = models.ArrayElement(models.ResourceElement())
+    incorrect_pictures = models.ArrayElement(models.ResourceElement(),
+                                             min_size=3, max_size=3)
 register_model(PictureChoice)
