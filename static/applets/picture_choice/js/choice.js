944f5d56821d43dccf5ac0594388399838b381cb
Jim Garrison <jim@garrison.cc>
2009-02-22 04:18:29 -0500

    picture_choice_lesson: correct/incorrect count

--- static/applets/picture_choice/js/choice.js
+++ static/applets/picture_choice/js/choice.js
@@ -1,9 +1,10 @@
 var picture_choice_correct_callbacks = [];
+var picture_choice_incorrect_callbacks = [];
 
 var pc_connect_click_actions = function () {
     $(".picture_choice_picture * img").click(function () {
+        var already_called = false;
         if ($(this).parent().parent().hasClass("correct")) {
-            var already_called = false;
             $('.ductus_picture_choice table tr td').not('.correct').hide(500, function () {
                 if (!already_called) {
                     $.each(picture_choice_correct_callbacks, function(i, cb) { cb(); });
@@ -11,7 +12,12 @@ var pc_connect_click_actions = function () {
                 already_called = true;
             });
         } else {
-            $(this).parent().fadeTo(200, .25);
+            $(this).parent().fadeTo(200, .25, function () {
+                if (!already_called) {
+                    $.each(picture_choice_incorrect_callbacks, function(i, cb) { cb(); });
+                }
+                already_called = true;
+            });
         }
     });
 };
