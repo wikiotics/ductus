9fd575cadb324e54a3ee61b4abbfd7858a8fe5a4
Jim Garrison <jim@garrison.cc>
2009-03-22 14:34:40 -0400

    map licenses to human-readable names when possible

---
+++ ductus/license/__init__.py
@@ -0,0 +1,13 @@
+# fixme: expand this list; consider marking for translation
+
+license_name_map = {
+    'http://creativecommons.org/licenses/publicdomain/': 'Public Domain',
+    'http://creativecommons.org/licenses/by-sa/1.0/': 'Creative Commons Attribution-ShareAlike 1.0',
+    'http://creativecommons.org/licenses/by-sa/2.0/': 'Creative Commons Attribution-ShareAlike 2.0',
+    'http://creativecommons.org/licenses/by-sa/2.5/': 'Creative Commons Attribution-ShareAlike 2.5',
+    'http://creativecommons.org/licenses/by-sa/3.0/': 'Creative Commons Attribution-ShareAlike 3.0',
+    'http://creativecommons.org/licenses/by/1.0/': 'Creative Commons Attribution 1.0',
+    'http://creativecommons.org/licenses/by/2.0/': 'Creative Commons Attribution 2.0',
+    'http://creativecommons.org/licenses/by/2.5/': 'Creative Commons Attribution 2.5',
+    'http://creativecommons.org/licenses/by/3.0/': 'Creative Commons Attribution 3.0',
+}
---
+++ ductus/wiki/templatetags/license_name.py
@@ -0,0 +1,33 @@
+# Ductus
+# Copyright (C) 2009  Jim Garrison <jim@garrison.cc>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.template.defaultfilters import stringfilter
+from django.template import Library
+
+from ductus.license import license_name_map
+
+register = Library()
+
+@register.filter
+@stringfilter
+def license_name(value):
+    """Resolves a URN, returning its absolute URL on the server
+    """
+
+    try:
+        return license_name_map[value]
+    except KeyError:
+        return value
