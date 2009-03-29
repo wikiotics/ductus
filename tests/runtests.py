b6f54febebc66c100d873bb2e06377081d9094fb
Jim Garrison <jim@garrison.cc>
2009-03-29 10:49:15 -0400

    removed ductus.util.xml
    (it is no longer imported anywhere)

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import unittest, doctest
-import ductus.util, ductus.util.xml
+import ductus.util
 
 suite = unittest.TestSuite()
-for mod in ductus.util, ductus.util.xml:
+for mod in ductus.util,:
     suite.addTest(doctest.DocTestSuite(mod))
 runner = unittest.TextTestRunner()
 runner.run(suite)
