7f6be71f11c8b01cc5b194df30a3941ff3de70ec
Jim Garrison <jim@garrison.cc>
2009-04-16 21:15:51 -0400

    picture_choice interface: once a picture is answered correctly, disable the click action

--- static/modules/picture_choice/js/choice.js
+++ static/modules/picture_choice/js/choice.js
@@ -1,8 +1,14 @@
 var picture_choice_correct_callbacks = [];
 var picture_choice_incorrect_callbacks = [];
+var picture_choice_actions_enabled = false;
 
 var pc_connect_click_actions = function () {
+    picture_choice_actions_enabled = true;
     $(".picture_choice_picture * img").click(function () {
+        if (!picture_choice_actions_enabled)
+            return;
+        picture_choice_actions_enabled = false;
+
         var already_called = false;
         if ($(this).parent().parent().hasClass("correct")) {
             $('.ductus_picture_choice table tr td').not('.correct').hide(500, function () {
