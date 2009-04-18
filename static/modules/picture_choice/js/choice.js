wikiotics/ductus

commit 7f6be71f11c8b01cc5b194df30a3941ff3de70ec

picture_choice interface: once a picture is answered correctly, disable the click action

static/modules/picture_choice/js/choice.js
@@ -1,8 +1,14 @@
 var picture_choice_correct_callbacks = [];
 var picture_choice_incorrect_callbacks = [];
+var picture_choice_actions_enabled = false;
 
 var pc_connect_click_actions = function () {
+    picture_choice_actions_enabled = true;
     $(".picture_choice_picture * img").click(function () {
+        if (!picture_choice_actions_enabled)
+            return;
+        picture_choice_actions_enabled = false;
+
         var already_called = false;
         if ($(this).parent().parent().hasClass("correct")) {
             $('.ductus_picture_choice table tr td').not('.correct').hide(500, function () {

commit e352dce2f4f44a5eaad865756f7bcf8c48628b46

Fixed regression introduced in [7f6be71f11c8b01cc5b194df30a3941ff3de70ec]
Thanks to Ian Sullivan for reporting the new bug.  Thanks to Teresa
Garrison for reporting the original bug.

static/modules/picture_choice/js/choice.js
@@ -7,10 +7,10 @@ var pc_connect_click_actions = function () {
     $(".picture_choice_picture * img").click(function () {
         if (!picture_choice_actions_enabled)
             return;
-        picture_choice_actions_enabled = false;
 
         var already_called = false;
         if ($(this).parent().parent().hasClass("correct")) {
+            picture_choice_actions_enabled = false;
             $('.ductus_picture_choice table tr td').not('.correct').hide(500, function () {
                 if (!already_called) {
                     $.each(picture_choice_correct_callbacks, function(i, cb) { cb(); });