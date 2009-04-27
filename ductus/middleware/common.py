c0c623eb6161538c24353cd15b3c89267e24dc79
Jim Garrison <jim@garrison.cc>
2009-04-26 23:20:30 -0400

    Fixed #32 by introducting request.escaped_full_path

--- ductus/middleware/common.py
+++ ductus/middleware/common.py
@@ -1,7 +1,15 @@
+from django.utils.encoding import iri_to_uri
+from django.utils.http import urlquote
+
 from ductus.wiki import SuccessfulEditRedirect
 from ductus.util.http import render_json_response
 
 class DuctusCommonMiddleware(object):
+    def process_request(self, request):
+        request.escaped_full_path = iri_to_uri(urlquote(request.path))
+        if request.META.get("QUERY_STRING", ''):
+            request.escaped_full_path += u'?' + iri_to_uri(request.META["QUERY_STRING"])
+
     def process_response(self, request, response):
         if request.is_ajax() and isinstance(response, SuccessfulEditRedirect):
             response = render_json_response({"urn": response.urn})
