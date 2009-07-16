1f1d23ade228a83446774e0222365d431493c0d2
Jim Garrison <jim@jimgarrison.org>
2009-07-15 20:56:04 -0400

    translate license names

--- ductus/license/__init__.py
+++ ductus/license/__init__.py
@@ -1,13 +1,13 @@
-# fixme: expand this list; consider marking for translation
+from django.utils.translation import ugettext_lazy as _
 
 license_name_map = {
-    'http://creativecommons.org/licenses/publicdomain/': 'Public Domain',
-    'http://creativecommons.org/licenses/by-sa/1.0/': 'Creative Commons Attribution-ShareAlike 1.0',
-    'http://creativecommons.org/licenses/by-sa/2.0/': 'Creative Commons Attribution-ShareAlike 2.0',
-    'http://creativecommons.org/licenses/by-sa/2.5/': 'Creative Commons Attribution-ShareAlike 2.5',
-    'http://creativecommons.org/licenses/by-sa/3.0/': 'Creative Commons Attribution-ShareAlike 3.0',
-    'http://creativecommons.org/licenses/by/1.0/': 'Creative Commons Attribution 1.0',
-    'http://creativecommons.org/licenses/by/2.0/': 'Creative Commons Attribution 2.0',
-    'http://creativecommons.org/licenses/by/2.5/': 'Creative Commons Attribution 2.5',
-    'http://creativecommons.org/licenses/by/3.0/': 'Creative Commons Attribution 3.0',
+    'http://creativecommons.org/licenses/publicdomain/': _('Public Domain'),
+    'http://creativecommons.org/licenses/by-sa/1.0/': _('Creative Commons Attribution-ShareAlike 1.0'),
+    'http://creativecommons.org/licenses/by-sa/2.0/': _('Creative Commons Attribution-ShareAlike 2.0'),
+    'http://creativecommons.org/licenses/by-sa/2.5/': _('Creative Commons Attribution-ShareAlike 2.5'),
+    'http://creativecommons.org/licenses/by-sa/3.0/': _('Creative Commons Attribution-ShareAlike 3.0'),
+    'http://creativecommons.org/licenses/by/1.0/': _('Creative Commons Attribution 1.0'),
+    'http://creativecommons.org/licenses/by/2.0/': _('Creative Commons Attribution 2.0'),
+    'http://creativecommons.org/licenses/by/2.5/': _('Creative Commons Attribution 2.5'),
+    'http://creativecommons.org/licenses/by/3.0/': _('Creative Commons Attribution 3.0'),
 }
