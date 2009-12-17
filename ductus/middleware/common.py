38e5a398cf12e31edf0502fc1175a5a3f2b27a5a
Jim Garrison <jim@jimgarrison.org>
2009-12-17 17:45:53 -0500

    Some docstrings
    (I made these changes long ago and apparently never committed them)

--- ductus/middleware/common.py
+++ ductus/middleware/common.py
@@ -5,16 +5,21 @@ from ductus.wiki import SuccessfulEditRedirect
 from ductus.util.http import render_json_response, ImmediateResponse
 
 class DuctusCommonMiddleware(object):
+    "Utility middleware for common Ductus tricks"
+
     def process_request(self, request):
+        "Sets request.escaped_full_path on each request"
         request.escaped_full_path = iri_to_uri(urlquote(request.path))
         if request.META.get("QUERY_STRING", ''):
             request.escaped_full_path += u'?' + iri_to_uri(request.META["QUERY_STRING"])
 
     def process_response(self, request, response):
+        "Handles successful ajax edits"
         if request.is_ajax() and isinstance(response, SuccessfulEditRedirect):
             response = render_json_response({"urn": response.urn})
         return response
 
     def process_exception(self, request, exception):
+        "Handles ImmediateResponse exception"
         if isinstance(exception, ImmediateResponse):
             return exception.response
