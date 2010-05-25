21822d79fbc5ef7819f3b73f7c81c1c18cd0a0c6
Jim Garrison <jim@jimgarrison.org>
2010-05-24 21:43:27 -0700

    make request.escaped_full_path a function, with the ability to override GET parameters

--- ductus/middleware/common.py
+++ ductus/middleware/common.py
@@ -1,17 +1,12 @@
 from django.utils.encoding import iri_to_uri
 from django.utils.http import urlquote
+from django.http import HttpRequest
 
 from ductus.wiki import SuccessfulEditRedirect
 from ductus.util.http import render_json_response, ImmediateResponse
 
 class DuctusCommonMiddleware(object):
     "Utility middleware for common Ductus tricks"
-
-    def process_request(self, request):
-        "Sets request.escaped_full_path on each request"
-        request.escaped_full_path = iri_to_uri(urlquote(request.path))
-        if request.META.get("QUERY_STRING"):
-            request.escaped_full_path += u'?' + iri_to_uri(request.META["QUERY_STRING"])
 
     def process_response(self, request, response):
         "Handles successful ajax edits"
@@ -23,3 +18,16 @@ class DuctusCommonMiddleware(object):
         "Handles ImmediateResponse exception"
         if isinstance(exception, ImmediateResponse):
             return exception.response
+
+def __escaped_full_path(request, **kwargs):
+    path = iri_to_uri(urlquote(request.path))
+    if kwargs:
+        GET = request.GET.copy()
+        for k, v in kwargs.items():
+            GET[k] = v
+        if GET:
+            path += u'?' + GET.urlencode()
+    elif request.META.get("QUERY_STRING"):
+        path += u'?' + iri_to_uri(request.META["QUERY_STRING"])
+    return path
+HttpRequest.escaped_full_path = __escaped_full_path
