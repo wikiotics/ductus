5535c17fa02fc7c52552ef42e43b2181b3f59221
Jim Garrison <jim@jimgarrison.org>
2010-05-24 21:27:36 -0700

    stupid simplification

--- ductus/middleware/common.py
+++ ductus/middleware/common.py
@@ -10,7 +10,7 @@ class DuctusCommonMiddleware(object):
     def process_request(self, request):
         "Sets request.escaped_full_path on each request"
         request.escaped_full_path = iri_to_uri(urlquote(request.path))
-        if request.META.get("QUERY_STRING", ''):
+        if request.META.get("QUERY_STRING"):
             request.escaped_full_path += u'?' + iri_to_uri(request.META["QUERY_STRING"])
 
     def process_response(self, request, response):
