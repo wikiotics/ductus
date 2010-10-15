09a2c7b9b15bbb6eba191e77371c0fd9546979b7
Jim Garrison <garrison@wikiotics.org>
2010-10-14 20:40:59 -0700

    use a better UI widget for editing group membership in the django admin

--- ductus/group/admin.py
+++ ductus/group/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from ductus.group.models import Group
 
-admin.site.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('users',)
+
+admin.site.register(Group, GroupAdmin)
