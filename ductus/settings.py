695db5a43b2182feffe245cd44cfbdf71ce6469c
Jim Garrison <garrison@wikiotics.org>
2010-10-27 00:33:22 -0700

    use UTC by default

--- ductus/settings.py
+++ ductus/settings.py
@@ -22,7 +22,7 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-TIME_ZONE = None
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
