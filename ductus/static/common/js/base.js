66601831e7c5b98ba0fda4fcfc043725b765f776
Jim Garrison <garrison@wikiotics.org>
2011-02-25 00:20:31 -0800

    added CSRF handling for ajax requests, according to feb 8 django security announcement

--- ductus/static/common/js/base.js
+++ ductus/static/common/js/base.js
@@ -4,6 +4,18 @@ function resolve_urn(urn) {
 
 $(function () {
 
+    // CSRF handling for AJAX requests
+    // (see http://www.djangoproject.com/weblog/2011/feb/08/security/)
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken",
+                                     $("#csrfmiddlewaretoken").val());
+            }
+        }
+    });
+
     // AJAX status indicator
     $("body").append($('<div id="ductus_ajax_status">Working...</div>').hide().ajaxStart(function () {
         $(this).show();
