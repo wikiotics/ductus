wikiotics/ductus

commit c17c26b0d56569015f58ecb2e644f150524d5014

moved split_urn to ductus.resource

ductus/resource/__init__.py
@@ -300,3 +300,32 @@ def get_resource_database():
     return _resource_database
 
 _resource_database = None
+
+__b64_chrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+__contains_only_base64_chrs = sequence_contains_only(__b64_chrs)
+
+def split_urn(urn):
+    """Checks to make sure it is a URN we could possibly support
+
+    URN must be in form 'urn:hash_type:hash_value' with base64 characters.
+    Otherwise, UnsupportedURN will be raised as an exception.
+
+    Returns (hash_type, digest) as a tuple.
+    """
+
+    if not isinstance(urn, basestring):
+        raise UnsupportedURN(urn)
+
+    urn_split = urn.split(':')
+    if len(urn_split) != 3:
+        raise UnsupportedURN(urn)
+
+    urn_str, hash_type, digest = urn_split
+    if not (urn_str == 'urn'
+            and hash_type != ''
+            and digest != ''
+            and __contains_only_base64_chrs(hash_type)
+            and __contains_only_base64_chrs(digest)):
+        raise UnsupportedURN(urn)
+
+    return hash_type, digest

ductus/resource/storage/local.py
@@ -17,7 +17,7 @@
 import os
 from shutil import copyfile
 
-from ductus.resource import UnsupportedURN
+from ductus.resource import split_urn, UnsupportedURN
 from ductus.util import iterate_file, sequence_contains_only, BLOCK_SIZE
 
 class LocalStorageBackend(object):
@@ -115,33 +115,3 @@ class LocalStorageBackend(object):
                         pass
 
     __iter__ = iterkeys
-
-
-__b64_chrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
-__contains_only_base64_chrs = sequence_contains_only(__b64_chrs)
-
-def split_urn(urn):
-    """Checks to make sure it is a URN we can support
-
-    URN must be in form 'urn:hash_type:hash_value' with base64 characters.
-    Otherwise, UnsupportedURN will be raised as an exception.
-
-    Returns (hash_type, digest) as a tuple.
-    """
-
-    if not isinstance(urn, basestring):
-        raise UnsupportedURN(urn)
-
-    urn_split = urn.split(':')
-    if len(urn_split) != 3:
-        raise UnsupportedURN(urn)
-
-    urn_str, hash_type, digest = urn_split
-    if not (urn_str == 'urn'
-            and hash_type != ''
-            and digest != ''
-            and __contains_only_base64_chrs(hash_type)
-            and __contains_only_base64_chrs(digest)):
-        raise UnsupportedURN(urn)
-
-    return hash_type, digest

ductus/wiki/mediacache.py
@@ -21,7 +21,7 @@ from shutil import copyfile
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-from ductus.resource.storage.local import split_urn
+from ductus.resource import split_urn
 from ductus.util import iterator_to_tempfile
 from ductus.wiki import registered_mediacache_views, get_resource_database
 from ductus.decorators import unvarying

commit 5532c21c743408bbec921a26f0a21d5c69367f53

fixed regression in c17c26b0d56569015f58ecb2e644f150524d5014; look to ductus.resource for get_resource_database()

ductus/modules/picture/forms.py
@@ -21,7 +21,8 @@ from django import forms
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy, ugettext as _
 
-from ductus.wiki import get_resource_database, resolve_urn, UnsupportedURN
+from ductus.resource import get_resource_database, UnsupportedURN
+from ductus.wiki import resolve_urn
 from ductus.modules.picture.ductmodels import Picture
 
 class PictureRotationForm(forms.Form):

ductus/resource/__init__.py
@@ -21,7 +21,7 @@ import os
 
 from lxml import etree
 
-from ductus.util import iterator_to_tempfile, create_property
+from ductus.util import iterator_to_tempfile, create_property, sequence_contains_only
 
 hash_name = "sha384"
 hash_encode = base64.urlsafe_b64encode

ductus/resource/storage/local.py
@@ -18,7 +18,7 @@ import os
 from shutil import copyfile
 
 from ductus.resource import split_urn, UnsupportedURN
-from ductus.util import iterate_file, sequence_contains_only, BLOCK_SIZE
+from ductus.util import iterate_file, BLOCK_SIZE
 
 class LocalStorageBackend(object):
     """Local storage backend.

ductus/wiki/mediacache.py
@@ -21,9 +21,9 @@ from shutil import copyfile
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-from ductus.resource import split_urn
+from ductus.resource import get_resource_database, split_urn
 from ductus.util import iterator_to_tempfile
-from ductus.wiki import registered_mediacache_views, get_resource_database
+from ductus.wiki import registered_mediacache_views
 from ductus.decorators import unvarying
 
 # the whole mediacache subsystem currently assumes that there are no illegal

ductus/wiki/models.py
@@ -21,7 +21,7 @@ from django.utils.encoding import iri_to_uri
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.utils.http import urlquote
 
-from ductus.wiki import get_resource_database
+from ductus.resource import get_resource_database
 from ductus.wiki.namespaces import split_pagename
 
 class WikiPage(models.Model):

ductus/wiki/views.py
@@ -29,8 +29,8 @@ from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.conf import settings
 
-from ductus.resource import determine_header
-from ductus.wiki import get_resource_database, registered_views, registered_creation_views, SuccessfulEditRedirect, resolve_urn, is_legal_wiki_pagename, user_has_edit_permission, user_has_unlink_permission
+from ductus.resource import get_resource_database, determine_header
+from ductus.wiki import registered_views, registered_creation_views, SuccessfulEditRedirect, resolve_urn, is_legal_wiki_pagename, user_has_edit_permission, user_has_unlink_permission
 from ductus.wiki.namespaces import BaseWikiNamespace, registered_namespaces, split_pagename, join_pagename, WikiPrefixNotProvided
 from ductus.wiki.models import WikiPage, WikiRevision
 from ductus.wiki.decorators import register_view