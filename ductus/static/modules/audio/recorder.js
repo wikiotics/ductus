56877e5c37b0950f0e259474ca68e06ff2e1acf0
Jim Garrison <garrison@wikiotics.org>
2012-03-20 18:50:21 -0700

    clarify that base64 encoding is licensed under gplv2 *or later*
    (since ductus is gplv3 or later, this is necessary for license
    compatibility)

--- ductus/static/modules/audio/recorder.js
+++ ductus/static/modules/audio/recorder.js
@@ -110,7 +110,17 @@ Wami.prototype.params = {
 }
 /*
  * Base64 encode/decode from http://ostermiller.org/calc/encode.html
- * Licensed under GPL version 2
+ * Copyright Stephen Ostermiller 2003-2006
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  */
 var END_OF_INPUT = -1;
 var base64Chars = new Array(
