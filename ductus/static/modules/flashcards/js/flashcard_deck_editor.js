wikiotics/ductus

commit 57a001af5787cb419b3cb98dc333e458ba533c1a

use Array.join() to join strings
in particular, due to the way strings are implemented in javascript,
joining strings the explicit way requires O(n**2) operations instead
of O(n) operations.  (Each time a string is appended, a new string must
be formed and all the characters thus far copied.  Array.join() avoids this.)

ductus/static/modules/flashcards/js/flashcard_deck_editor.js
@@ -897,13 +897,7 @@ $(function () {
         // the widget used to edit tags applied to the whole flashcard deck
         ModelWidget.call(this, tags, '<div id="ductus_TaggingWidget"><label>' + gettext('Tags (space separated):') + '</label><input /></div>');
         this.input = this.elt.children('input');
-        var tag_string = '';
-        if (tags) {
-            for (var i = 0; i < tags.array.length; ++i) {
-                var tag = tags.array[i];
-                tag_string += tag.value + ' ';
-            }
-        }
+        var tag_string = tags ? tags.array.join(' ') : '';
         this.input.val(tag_string);
     }
     TaggingWidget.prototype = chain_clone(Widget.prototype);

commit f654b2bb9abdbd51f74d5a2327f006db08bdc756

fix bug introduced in 57a001af5787cb419b3cb98dc333e458ba533c1a
tags.array.join() joined tag object not their values.
We do not use tags.array.map() here since it would break IE6-7 compat.
(according to http://kangax.github.com/es5-compat-table/#showold)

ductus/static/modules/flashcards/js/flashcard_deck_editor.js
@@ -897,7 +897,14 @@ $(function () {
         // the widget used to edit tags applied to the whole flashcard deck
         ModelWidget.call(this, tags, '<div id="ductus_TaggingWidget"><label>' + gettext('Tags (space separated):') + '</label><input /></div>');
         this.input = this.elt.children('input');
-        var tag_string = tags ? tags.array.join(' ') : '';
+        var tag_string = '';
+        var tag_names = [];
+        if (tags) {
+            for (var i = 0, l = tags.array.length; i < l; ++i) {
+                tag_names.push(tags.array[i].value);
+            }
+            tag_string = tag_names.join(' ');
+        }
         this.input.val(tag_string);
     }
     TaggingWidget.prototype = chain_clone(Widget.prototype);