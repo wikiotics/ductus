8b65d6aefcf13b13fbea50fa0e7a62b4bc247e15
Jim Garrison <garrison@wikiotics.org>
2012-04-30 19:25:31 -0700

    migrate to pytest

--- runtests.py
+++ runtests.py
@@ -2,20 +2,9 @@
 
 # We use our own test script for now, until Django #10853 is fixed.
 
-import os
-
 if __name__ == "__main__":
+    import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ductus.settings")
 
-    import ductus.util
-    import ductus.util.bcp47
-    import ductus.license
-
-    import unittest, doctest
-
-    suite = unittest.TestSuite()
-    for mod in ductus.util, ductus.util.bcp47, ductus.license:
-        suite.addTest(doctest.DocTestSuite(mod))
-
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    import pytest
+    pytest.main(["ductus", "--doctest-modules"])
