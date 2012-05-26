6472a64c2e41e2262357a0ecba8cf031cbed7f1b
Jim Garrison <garrison@wikiotics.org>
2012-05-25 20:55:47 -0700

    in runtests.py, import ductus.initialize

--- runtests.py
+++ runtests.py
@@ -3,8 +3,11 @@
 # We use our own test script for now, until Django #10853 is fixed.
 
 if __name__ == "__main__":
+    # initialize ductus
     import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ductus.settings")
+    import ductus.initialize
 
+    # perform the tests
     import pytest
     pytest.main(["--doctest-modules", "ductus", "tests"])
