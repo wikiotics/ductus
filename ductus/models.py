048bc8373645e7b4b4ae61a90a1f246c6598a348
Jim Garrison <garrison@wikiotics.org>
2012-06-20 01:48:19 -0700

    added some documentation notes

---
+++ ductus/models.py
@@ -0,0 +1,2 @@
+# this file is necessary for tests to run, though it can be empty
+# (see http://stackoverflow.com/a/6964563)
--- ductus/tests/__init__.py
+++ ductus/tests/__init__.py
@@ -1,1 +1,4 @@
+# list all test modules to be run by 'manage.py test ductus'
+# (see http://stackoverflow.com/a/5160779)
+
 from modules.flashcards.flashcard_deck import *
