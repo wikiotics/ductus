wikiotics/ductus

commit 8240c71e2dcfc4ffd8581de55447d6cc205c993a

cancel recording if a new recording is started while recording

ductus/static/common/js/editing-widgets.js
@@ -984,6 +984,9 @@ function OnlineRecorder() {
 OnlineRecorder.prototype = chain_clone(Widget.prototype);
 OnlineRecorder.prototype.init = function() {
     // set or reset the widget to its initial status: upload or record buttons
+    if (this.Wami) {
+        this.cancelRecording();   // ensure there's no recording ongoing if the user forgets to stop it before creating some audio elsewhere in a page
+    }
     this.hideButtons();
     if (this.record_btn) {
         this.listen(false);
@@ -1091,6 +1094,10 @@ OnlineRecorder.prototype.stopRecording = function() {
     this.showRecordButton('start');
     this.Wami.stopRecording();
 };
+OnlineRecorder.prototype.cancelRecording = function() {
+    this.cancel_recording = true;
+    this.Wami.stopRecording();
+}
 /**
  * Callbacks from the flash indicating certain events
  */
@@ -1103,7 +1110,11 @@ OnlineRecorder.prototype.onRecordStart = function() {
 };
 OnlineRecorder.prototype.onRecordFinish = function() {
     console.log('OR onRecordFinish');
-    this.uploadAudio();
+    if (!this.cancel_recording) {
+        this.uploadAudio();
+    } else {
+        delete(this.cancel_recording);
+    }
 };
 
 function TaggingWidget(tags, fcdw) {

commit 44680f4bad76ab139cb4f11b9f00726ab9f6f069

allow multiple recordings to upload
bug introduced in 8240c71e2dcfc4ffd8581de55447d6cc205c993a

ductus/static/common/js/editing-widgets.js
@@ -984,7 +984,7 @@ function OnlineRecorder() {
 OnlineRecorder.prototype = chain_clone(Widget.prototype);
 OnlineRecorder.prototype.init = function() {
     // set or reset the widget to its initial status: upload or record buttons
-    if (this.Wami) {
+    if (this.Wami && this.recording) {
         this.cancelRecording();   // ensure there's no recording ongoing if the user forgets to stop it before creating some audio elsewhere in a page
     }
     this.hideButtons();
@@ -1092,6 +1092,7 @@ OnlineRecorder.prototype.uploadAudio = function() {
 OnlineRecorder.prototype.startRecording = function() {
     this.showRecordButton('stop');
     this.Wami.startRecording("", "online_recorder.onRecordStart", "online_recorder.onRecordFinish", "online_recorder.onError");
+    this.recording = true;
 };
 OnlineRecorder.prototype.stopRecording = function() {
     this.showRecordButton('start');
@@ -1113,6 +1114,7 @@ OnlineRecorder.prototype.onRecordStart = function() {
 };
 OnlineRecorder.prototype.onRecordFinish = function() {
     console.log('OR onRecordFinish');
+    this.recording = false;
     if (!this.cancel_recording) {
         this.uploadAudio();
     } else {