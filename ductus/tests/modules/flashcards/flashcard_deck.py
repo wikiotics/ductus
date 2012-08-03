3aac476559b80e712599c404d425e01cbe877d42
Laurent Savaete <laurent.savaete@gmail.com>
2012-08-03 19:14:53 +0200

    test that save widget is present on new empty wikitext page

--- ductus/tests/modules/flashcards/flashcard_deck.py
+++ ductus/tests/modules/flashcards/flashcard_deck.py
@@ -29,3 +29,9 @@ class FlashcardDeckTests(LiveServerTestCase):
         self.selenium.get('%s%s' % (self.live_server_url, '/new/flashcard_deck'))
         w = self.selenium.find_elements_by_class_name('ductus_SaveWidget')
         assert len(w) > 0, "SaveWidget not found"
+
+    def test_wikitext_save_widget(self):
+        # check that the save widget is present when creating a new wikitext page from scratch
+        self.selenium.get('%s%s' % (self.live_server_url, '/new/wikitext'))
+        w = self.selenium.find_elements_by_class_name('ductus_SaveWidget')
+        assert len(w) > 0, "SaveWidget not found"
