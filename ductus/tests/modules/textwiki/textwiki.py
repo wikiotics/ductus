4b7c96402c77736a681e84a293a2e17da9d64e7f
Laurent Savaete <laurent.savaete@gmail.com>
2012-08-17 16:35:43 +0200

    test save + view support for creole in backend

--- ductus/tests/modules/textwiki/textwiki.py
+++ ductus/tests/modules/textwiki/textwiki.py
@@ -1,3 +1,5 @@
+from django.test import TestCase
+from django.test.client import Client
 from ductus.tests.basecase import JSTestCase
 
 class TextwikiTests(JSTestCase):
@@ -7,3 +9,17 @@ class TextwikiTests(JSTestCase):
         self.selenium.get('%s%s' % (self.live_server_url, '/new/wikitext'))
         w = self.selenium.find_elements_by_class_name('ductus_SaveWidget')
         assert len(w) > 0, "SaveWidget not found"
+
+class TextWikiBackendTests(TestCase):
+    def test_textwiki_save_and_view_creole_page(self):
+        """send a simple creole blueprint to the server and check the resulting displayed content"""
+        c = Client()
+        target_url = '/hr/sometest_textpage'
+        POST = {
+            'blueprint': '{"resource":{"blob":{"text":"= test title =\\n* element number one\\n* element number two\\n* element number three","markup_language":"creole-1.0"},"tags":{"array":[]},"@create":"{http://wikiotics.org/ns/2009/wikitext}wikitext"}}',
+            'log_message': 'created from unit tests'
+        }
+        response = c.post(target_url, POST, follow=True)
+        self.assertRedirects(response, target_url)  # check that we got a 302 redirect plus a 200 to target_url
+        self.assertContains(response, '<h1>test title</h1>', html=True)
+        self.assertContains(response, '<li>element number two</li>', html=True)
