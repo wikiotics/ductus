7a4da484ae856e23b8c2e151efd1501f8a3cf8a6
Laurent Savaete <laurent.savaete@gmail.com>
2012-08-26 17:51:50 +0200

    refactor text wiki test code

--- ductus/tests/modules/textwiki/textwiki.py
+++ ductus/tests/modules/textwiki/textwiki.py
@@ -2,6 +2,23 @@ from django.test import TestCase
 from django.test.client import Client
 from ductus.tests.basecase import JSTestCase
 
+def post_text_blueprint(url, wikitext, markup_language):
+    """POST a blueprint for a text page to the server and return the response"""
+    c = Client()
+    POST = {
+        'blueprint': '{"resource":{"blob":{"text":"' + wikitext + '","markup_language":"' + markup_language + '"},"tags":{"array":[]},"@create":"{http://wikiotics.org/ns/2009/wikitext}wikitext"}}',
+        'log_message': 'created from unit tests'
+    }
+    return c.post(url, POST, follow=True)
+
+def post_creole_blueprint(url, creole_text):
+    """POST a creole text wiki blueprint and return the result"""
+    return post_text_blueprint(url, creole_text, 'creole-1.0')
+
+def post_html5_blueprint(url, html_text):
+    """POST a ductus-html5 text wiki blueprint and return the result"""
+    return post_text_blueprint(url, html_text, 'ductus-html5')
+
 class TextwikiTests(JSTestCase):
 
     def test_wikitext_save_widget(self):
@@ -13,20 +30,15 @@ class TextwikiTests(JSTestCase):
 class TextWikiBackendTests(TestCase):
     def test_textwiki_save_and_view_creole_page(self):
         """send a simple creole blueprint to the server and check the resulting displayed content"""
-        c = Client()
         target_url = '/hr/sometest_textpage'
-        POST = {
-            'blueprint': '{"resource":{"blob":{"text":"= test title =\\n* element number one\\n* element number two\\n* element number three","markup_language":"creole-1.0"},"tags":{"array":[]},"@create":"{http://wikiotics.org/ns/2009/wikitext}wikitext"}}',
-            'log_message': 'created from unit tests'
-        }
-        response = c.post(target_url, POST, follow=True)
+        creole = "= test title =\\n* element number one\\n* element number two\\n* element number three"
+        response = post_creole_blueprint(target_url, creole)
         self.assertRedirects(response, target_url)  # check that we got a 302 redirect plus a 200 to target_url
         self.assertContains(response, '<h1>test title</h1>', html=True)
         self.assertContains(response, '<li>element number two</li>', html=True)
 
     def test_textwiki_save_and_view_html5_page(self):
         """send a simple ductus-html5 blueprint to the server and check the resulting displayed content"""
-        c = Client()
         target_url = '/hr/sometest_textpage'
         text = '<h1>test title</h1>\
                 <p>some text in a paragraph</p>\
@@ -34,11 +46,7 @@ class TextWikiBackendTests(TestCase):
                 <li>element number two</li>\
                 <li>element number three</li></ul>\
                 <h2>some other heading</h2>'
-        POST = {
-            'blueprint': '{"resource":{"blob":{"text":"' + text + '","markup_language":"ductus-html5"},"tags":{"array":[]},"@create":"{http://wikiotics.org/ns/2009/wikitext}wikitext"}}',
-            'log_message': 'created from unit tests'
-        }
-        response = c.post(target_url, POST, follow=True)
+        response = post_html5_blueprint(target_url, text)
         self.assertRedirects(response, target_url)  # check that we got a 302 redirect plus a 200 to target_url
         self.assertContains(response, '<h1>test title</h1>', html=True)
         self.assertContains(response, '<li>element number two</li>', html=True)
