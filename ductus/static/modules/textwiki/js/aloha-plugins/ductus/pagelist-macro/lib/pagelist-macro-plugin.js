4414612004783416c48a972f47f63b34427cc037
Laurent Savaete <laurent.savaete@gmail.com>
2012-09-13 15:40:17 +0200

    add a property field to edit pagelist macro tags

--- ductus/static/modules/textwiki/js/aloha-plugins/ductus/pagelist-macro/lib/pagelist-macro-plugin.js
+++ ductus/static/modules/textwiki/js/aloha-plugins/ductus/pagelist-macro/lib/pagelist-macro-plugin.js
@@ -21,13 +21,22 @@ define([
     return Plugin.create('ductus-pagelist-macro', {
 
         init: function() {
+            var that = this;
+            this._currentBlock = {};    // a reference to the currently selected block, as we only seem to be able to figure that from an activate event
+
             // init the pagelist plugin using an aloha block mechanism
             BlockManager.registerBlockType('DuctusPageListMacroBlock', ductusmacroblock.DuctusPageListMacroBlock);
 
             // make sure the BlockManager is aware of pre-existing macro marker when opening the editor
             Aloha.ready(function() {
                 Aloha.jQuery('div.ductus-macro').alohaBlock({
-                    'aloha-block-type': 'DuctusPageListMacroBlock'
+                    'aloha-block-type': 'DuctusPageListMacroBlock',
+                    'block-skip-scope':'true'
+                });
+                BlockManager.bind('block-activate', function (blocks) {
+                    that._currentBlock = blocks[0];
+                    that._tagField.setValue(that._currentBlock.attr('tags'));
+                    that._tagField.foreground();
                 });
             });
             this.initButtons();
@@ -43,12 +52,31 @@ define([
                 scope: 'Aloha.continuoustext',
                 click: function () { that.insertMacroBlock(); }
             });
+            this._tagField = AttributeField({
+                width: 250,
+                name: 'PageListTags',
+                scope: 'pagelisttags'
+            });
+
+            this._tagField.addListener( 'keyup', function ( event ) {
+
+                if ( event.keyCode == 13 ) {
+                    // the user hit 'enter': remove the macro marker if the input field is empty
+                    var val = that._tagField.getValue();
+                    if (val == '') {
+                        window.console.log('remove macro');
+                    } else {
+                        that._currentBlock.attr('tags', val);
+                    }
+                }
+            });
         },
 
         insertMacroBlock: function() {
             // this is class when the user clicks the macro button in the aloha toolbar
 
             var marker = Aloha.jQuery('<div class="ductus-macro" data-macro-name="pagelist" data-tags="sometag"></div>');
+            marker.attr('data-block-skip-scope', 'true');   // prevent aloha from switching scope upon activation of a marker
             var range = Aloha.Selection.getRangeObject();
             var Editable = Aloha.activeEditable;
             var Container = Aloha.jQuery(document.getElementById(Editable.getId()));
@@ -56,6 +84,9 @@ define([
             marker.alohaBlock({
                 'aloha-block-type': 'DuctusPageListMacroBlock'
             });
+            this._tagField.setValue(BlockManager.getBlock(marker).attr('tags'));
+            this._tagField.foreground();
+            this._tagField.focus();
         },
 
     });
--- ductus/static/modules/textwiki/js/textwiki_editor.js
+++ ductus/static/modules/textwiki/js/textwiki_editor.js
@@ -49,6 +49,7 @@ $(function() {
             markers.removeAttr('id')
                    .removeClass('aloha-block-DuctusPageListMacroBlock')
                    .removeAttr('data-aloha-block-type')
+                   .removeAttr('data-block-skip-scope')
                    .removeAttr('data-sortable-item');   // this attr seems to appear a bit randomly, we remove it, just in case
             // DO NOT remove the contenteditable attribute, it breaks the whole plugin
 
